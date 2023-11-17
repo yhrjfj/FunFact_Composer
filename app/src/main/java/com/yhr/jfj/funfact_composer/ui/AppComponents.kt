d1d6f100c12f8244894cb5c2e0a8795eb053270b
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-11-17 10:19:03 +0600

    TopBar

---
+++ app/src/main/java/com/yhr/jfj/funfact_composer/ui/AppComponents.kt
@@ -0,0 +1,49 @@
+package com.yhr.jfj.funfact_composer.ui
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.yhr.jfj.funfact_composer.R
+
+@Composable
+fun TopBar() {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(8.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = "Hi there",
+            color = Color.Black,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Medium
+        )
+        Spacer(modifier = Modifier.weight(1f))
+        Image(
+            painter = painterResource(id = R.drawable.yhrjfj),
+            contentDescription = "YHRJFJ",
+            modifier = Modifier.size(100.dp)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun TopBarPreview() {
+    TopBar()
+}
