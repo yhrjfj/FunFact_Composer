421e8475a522f91dc269654ec93171ba1c5920c7
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-11-19 19:44:08 +0600

    Hide keyboard or remove focus from keyboard

--- app/src/main/java/com/yhr/jfj/funfact_composer/ui/AppComponents.kt
+++ app/src/main/java/com/yhr/jfj/funfact_composer/ui/AppComponents.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
@@ -21,6 +22,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
@@ -89,6 +91,9 @@ fun TextFieldComponent(onTextChange: (name: String) -> Unit) {
     var currentValue by remember {
         mutableStateOf("")
     }
+
+    val localFocusManager = LocalFocusManager.current
+
     OutlinedTextField(
         modifier = Modifier.fillMaxWidth(),
         value = currentValue,
@@ -107,7 +112,10 @@ fun TextFieldComponent(onTextChange: (name: String) -> Unit) {
         ),
         keyboardOptions = KeyboardOptions(
             imeAction = ImeAction.Done
-        )
+        ),
+        keyboardActions = KeyboardActions {
+            localFocusManager.clearFocus()
+        }
     )
 }
 
