d4996077aeee8ecdc70dcd396a97ec5814f25369
RiJo <102893295+yhrjfj@users.noreply.github.com>
2023-11-19 18:52:54 +0600

    Navigation graph

--- app/src/main/java/com/yhr/jfj/funfact_composer/ui/screens/FunFaceNavigationGraph.kt
+++ app/src/main/java/com/yhr/jfj/funfact_composer/ui/screens/FunFaceNavigationGraph.kt
@@ -1,16 +1,18 @@
 package com.yhr.jfj.funfact_composer.ui.screens
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.yhr.jfj.funfact_composer.ui.UserInputViewModel
 
 @Composable
-fun FunFaceNavigationGraph() {
+fun FunFaceNavigationGraph(userInputViewModel: UserInputViewModel = viewModel()) {
     val navController = rememberNavController()
     NavHost(navController = navController, startDestination = Routes.USER_INPUT_SCREEN) {
         composable(Routes.USER_INPUT_SCREEN) {
-            UserInputScreen(navController)
+            UserInputScreen(userInputViewModel)
         }
         composable(Routes.WELCOME_SCREEN) {
             WelcomeScreen()
